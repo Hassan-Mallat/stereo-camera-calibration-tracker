4e0f4f2c67baab674d7220e70f47c08f09d80cc8
Hassan-Mallat <mallathasan003@gmail.com>
2026-07-19 14:36:37 +0300

    Add files via upload

---
+++ main/code/prediction_triangulation.py
@@ -0,0 +1,259 @@
+import cv2 as cv
+import numpy as np
+
+def build_projection_matrix(calibration_file, image_path, target_id=2, marker_size_mm=500):
+    # Calculates rvec/tvec from the background image and builds the P matrix
+
+    '''
+    The Goal: Find exactly where the camera is in the 3D world,
+    and build a matrix that converts any 3D world point into a 2D pixel for that specific camera
+    '''
+
+    with np.load(calibration_file) as data:
+        camMatrix = data['camMatrix']
+        distCoeff = data['distCoeff']
+
+    half_size = marker_size_mm / 2.0
+    objp = np.array([
+        [-half_size,  half_size, 0],
+        [ half_size,  half_size, 0],
+        [ half_size, -half_size, 0],
+        [-half_size, -half_size, 0]
+    ], dtype=np.float32)
+
+    img = cv.imread(image_path)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
+    parameters = cv.aruco.DetectorParameters()
+    detector = cv.aruco.ArucoDetector(aruco_dict, parameters)
+    corners, ids, _ = detector.detectMarkers(gray)
+
+    # ID LOCKING
+    if ids is None:
+        raise ValueError(f"No ArUco markers detected in {image_path}!")
+
+    # Flatten the IDs array to make it a simple 1D list
+    ids_flat = ids.flatten()
+
+    # Check if our target World Origin marker is actually in the image (the aruco marker, we locked to the front one)
+    if target_id not in ids_flat:
+        raise ValueError(f"CRITICAL ERROR: Target ID {target_id} not found... The camera only sees these IDs: {ids_flat}")
+
+    # Find the exact index of our target marker
+    best_marker_index = np.where(ids_flat == target_id)[0][0]
+
+    # Lock onto its corners
+    master_corners = corners[best_marker_index][0] # X and Y pixel coordinates of the 4 outer corners of the ArUco square
+
+    _, rvec, tvec = cv.solvePnP(objp, master_corners, camMatrix, distCoeff)
+
+    R, _ = cv.Rodrigues(rvec) # rvec is the rotation vect (3x1) we convert it into a 3x3 rotation matrix R 
+    Rt = np.hstack((R, tvec)) # Mext matrix
+    P = camMatrix.dot(Rt) # Projection Matrix
+    '''multiplying P by any 3D real-world coordinate,
+    it will spit out the exact 2D pixel where that point will appear on the camera sensor'''
+    
+    return P, rvec, tvec, camMatrix, distCoeff
+
+
+def predict_cam2_from_cam1(u1, v1, Z_known, rvec1, tvec1, K1, dist1, rvec2, tvec2, K2, dist2):
+    # Predicts Cam 2 pixel using Cam 1 pixel and known Z
+    point_cam1 = np.array([[[float(u1), float(v1)]]])
+    undistorted = cv.undistortPoints(point_cam1, K1, dist1)
+    x_norm, y_norm = undistorted[0][0]
+    
+    ray_cam = np.array([[x_norm], [y_norm], [1.0]]) # 3D vector that starts inside the lens,
+    # shoots straight out, and passes exactly through the x_norm, y_norm point on that virtual screen 1 unit away
+    # successfully converted a 2D pixel into a 3D directional arrow pointing exactly at the student
+    
+
+    '''
+    The Problem: ray_cam is pointing at the student, but it is using the camera's local rotation
+    (where Z is "straight out the lens"). The rest of our math uses the ArUco marker's
+    world rotation (where Z is "straight up from the floor").
+
+    The Solution (ray_world): By multiplying the ray by the inverse rotation matrix (R1_inv),
+    you mathematically twist that 3D arrow so it aligns with the real-world X, Y, and Z axes.
+    ray_world is now the exact physical trajectory of the line of sight pointing from the camera
+    to the student.
+    '''
+    R1, _ = cv.Rodrigues(rvec1)
+    R1_inv = R1.T
+    ray_world = R1_inv @ ray_cam
+    
+    C1 = -R1_inv @ tvec1 # calculates Camera 1's exact optical center in 3D space. This is where the ray starts
+    
+    d_z = ray_world[2][0] # This is the Z-component of ray_world trajectory. It tells how fast the laser beam is moving downwards toward the floor for every step it takes forward
+    C1_z = C1[2][0]
+    
+    if abs(d_z) < 1e-6:
+        print("Ray is perfectly horizontal, cannot intersect Z-plane.")
+        return None
+    
+    
+    # We know the student is standing at a specific height off the floor (Z_known).
+    # We need to find exactly where the laser beam intersects the invisible flat plane hovering at Z_known
+    
+    '''
+    t : We need to know the total Z-distance the laser has to drop to get from the
+    camera's height (C1_z) down to the student's height (Z_known).
+    By dividing that total distance by the drop-rate (d_z), we calculate t.
+    t is exactly how many units the laser beam has to travel forward along the ray to hit the target
+    '''
+    t = (Z_known - C1_z) / d_z # Ray-Plane Intersection
+    '''
+    Now that we know the laser must travel distance t, we simply take the camera's starting location (C1)
+    and move forward by t along the X trajectory and Y trajectory
+    '''
+    X = C1[0][0] + t * ray_world[0][0]
+    Y = C1[1][0] + t * ray_world[1][0]
+    
+    point_3d = np.array([[[X, Y, Z_known]]], dtype=np.float32)
+    
+    pixels_cam2, _ = cv.projectPoints(point_3d, rvec2, tvec2, K2, dist2)
+    
+    u2 = int(pixels_cam2[0][0][0])
+    v2 = int(pixels_cam2[0][0][1])
+    
+    return u2, v2
+
+def get_fundamental_matrix(rvec1, tvec1, K1, rvec2, tvec2, K2):
+    '''
+    The Goal: Calculate the mathematical relationship between the two cameras so they understand they
+              are looking at the same 3D space
+    '''
+    # Calculates the Fundamental Matrix linking Cam 1 and Cam 2
+    R1, _ = cv.Rodrigues(rvec1)
+    R2, _ = cv.Rodrigues(rvec2)
+    
+    # R_rel : Relative Rotation
+    # T_rel : Relative Translation
+    R_rel = R2 @ R1.T # .T is transpose. we actually want to inverse the matrix R matrix but since it's orthogonal R.T = R^-1
+                      # but .T is less computing
+    T_rel = tvec2 - (R_rel @ tvec1)
+
+    '''
+    To build the Essential Matrix (E),
+    the geometric formula strictly requires you to calculate the Cross Product of the relative 
+    translation and relative rotation: E = T_rel * R_rel
+
+    but we cannot easily perform a cross product between a 1D vector T_rel and a 2D matrix R_rel
+    using standard matrix multiplication
+
+    Solution : In linear algebra, you can convert any cross product into a standard matrix multiplication
+    by turning the vector into a 3x3 skew-symmetric matrix. Tx is simply the T_rel vector stretched
+    out into a 3x3 grid with zeros down the middle.
+    By doing this, you can simply multiply them: E = Tx @ R_rel
+    '''
+    # Tx : Skew-Symmetric Matrix
+    Tx = np.array([
+        [0, -T_rel[2][0], T_rel[1][0]],
+        [T_rel[2][0], 0, -T_rel[0][0]],
+        [-T_rel[1][0], T_rel[0][0], 0]
+    ])
+    
+    E = Tx @ R_rel
+    
+    K2_inv = np.linalg.inv(K2)
+    K1_inv = np.linalg.inv(K1)
+    F = K2_inv.T @ E @ K1_inv
+    
+    F = F / F[2, 2]
+    return F
+
+
+
+# ==========================================
+# THE 2-CAMERA EXECUTION PIPELINE
+# ==========================================
+
+print("Initializing Camera Matrices (Front-Mid and Back)...")
+
+# Both cameras will now exclusively lock onto Marker ID 0
+P1, rvec1, tvec1, camMatrix1, distCoeff1 = build_projection_matrix(
+    'mid_calibration.npz', 'locking_images_middle_cam/frame_001.jpg', 
+    target_id=0, marker_size_mm=500
+)
+
+P2, rvec2, tvec2, camMatrix2, distCoeff2 = build_projection_matrix(
+    'back_calibration.npz', 'locking_images_back_cam/frame_028.jpg', 
+    target_id=0, marker_size_mm=500
+)
+
+# Check Camera 1's physical height
+R1, _ = cv.Rodrigues(rvec1)
+# C1 = -R1.T @ tvec1 outputs a 3x1 vector containing the exact physical [X, Y, Z] coordinates of the camera lens in millimeters
+C1_z = (-R1.T @ tvec1)[2][0] # this gives us the Z-coord. but since the aruco is on the floor (Z = 0), it'll give us the height of the cam
+print(f"Mathematical Front Cam Height: {C1_z:.0f} mm") 
+# This should print very close to 2430 mm (real life measurements i took myself)
+
+
+# Feed the RAW AI Pixel Data
+u_front = 1512.0 
+v_front = 746.0  
+u_back = 634.0   
+v_back = 255.0   
+
+point_front_cam = np.array([[[u_front, v_front]]], dtype=np.float32)
+point_back_cam = np.array([[[u_back, v_back]]], dtype=np.float32)
+
+# Mathematically flatten the pixels
+undistorted_front = cv.undistortPoints(point_front_cam, camMatrix1, distCoeff1, P=camMatrix1)
+undistorted_back = cv.undistortPoints(point_back_cam, camMatrix2, distCoeff2, P=camMatrix2)
+
+# Epipolar Snapping
+print("\nSnapping pixels to perfect Epipolar geometry...")
+F = get_fundamental_matrix(rvec1, tvec1, camMatrix1, rvec2, tvec2, camMatrix2)
+
+pts1_for_corr = undistorted_front.reshape(1, 1, 2)
+pts2_for_corr = undistorted_back.reshape(1, 1, 2)
+
+new_pts1, new_pts2 = cv.correctMatches(F, pts1_for_corr, pts2_for_corr)
+
+print(f"Original Front Pixel: {pts1_for_corr[0][0]}")
+print(f"Corrected Front Pixel: {new_pts1[0][0]}")
+print(f"Original Back Pixel: {pts2_for_corr[0][0]}")
+print(f"Corrected Back Pixel: {new_pts2[0][0]}")
+
+# Triangulate using the CORRECTED coordinates
+point_front_ready = new_pts1.reshape(2, 1)
+point_back_ready = new_pts2.reshape(2, 1)
+
+print("\nShooting perfectly aligned Epipolar Rays...")
+points_4D = cv.triangulatePoints(P1, P2, point_front_ready, point_back_ready) #  matrix with 4 rows (X, Y, Z, W) and N columns (representing however many students we are tracking)
+points_3D = points_4D[:3, :] / points_4D[3, :] # Normalization (De-Homogenization)
+# points_4D[:3, :]: This slices the matrix and grabs Row 0, 1, and 2 (The raw X, Y, and Z values for all students)
+# points_4D[3, :]: This slices the matrix and grabs exactly Row 3 (The W values for all students)
+# By dividing the top three rows by the bottom row, you mathematically crush the 4D coordinate back into our physical 3D reality, outputting the true points_3D (X, Y, Z)
+
+X = float(points_3D[0, 0])
+Y = float(points_3D[1, 0])
+Z = float(points_3D[2, 0])
+
+print("\n--- True Triangulated Real-World Location ---")
+print(f"X: {X:.0f} mm")
+print(f"Y: {Y:.0f} mm")
+print(f"Z: {Z:.0f} mm (Height from floor)")
+
+
+
+
+# ==========================================
+# TEST PREDICTION:
+# ==========================================
+print("\n--- Testing Single-Camera Fallback Prediction ---")
+
+# 1- We simulate the Back Camera failing.
+# 2- We use the True Z we just calculated (approx 691 mm).
+Z_known = Z 
+
+# 3- Predict where the desk should be in the blind Back Camera
+predicted_u_back, predicted_v_back = predict_cam2_from_cam1(
+    u_front, v_front, Z_known, 
+    rvec1, tvec1, camMatrix1, distCoeff1,  # Cam 1 (Front) Data
+    rvec2, tvec2, camMatrix2, distCoeff2   # Cam 2 (Back) Data
+)
+
+print(f"Prediction for Back Camera: (X: {predicted_u_back}, Y: {predicted_v_back})")
+print(f"Actual Pixel in Back Camera: (X: {u_back}, Y: {v_back})")
